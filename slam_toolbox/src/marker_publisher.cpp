4c0e3fe399883af504a72ba74a8d5aa1128ffc34
jsongCMU <jaekyuns@andrew.cmu.edu>
2022-09-25 18:46:20 -0400

    Commit to multiple topics; color map by tag ID

--- slam_toolbox/src/marker_publisher.cpp
+++ slam_toolbox/src/marker_publisher.cpp
@@ -3,7 +3,14 @@
 #include "apriltag_ros/AprilTagDetectionArray.h"
 #include "apriltag_ros/AprilTagDetection.h"
 
+// Agent info
+const std::vector<std::string> agent_names = {"agent1", "agent2"};
+// Topic info
+const std::string topic_name_prefix = "/robosar_agent_bringup_node/";
+const std::string topic_name_suffix = "/feedback/apriltag";
+
 ros::Publisher msg_pub;
+std::map<int, std_msgs::ColorRGBA> tag_color_map;
 
 void msgCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr& msg)
 {
@@ -30,11 +37,23 @@ void msgCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr& msg)
       m.scale.x = 0.3;
       m.scale.y = 0.3;
       m.scale.z = 0.3;
-      // color
-      m.color.r = 0.2;
-      m.color.g = 0.2;
-      m.color.b = 1;
-      m.color.a = 1;
+      // color - use previous color or create new one
+      if(tag_color_map.find(m.id) == tag_color_map.end())
+      {
+        // New tag; create and save color
+        std_msgs::ColorRGBA new_color;
+        new_color.r = (float)(rand() % 100) / 100;
+        new_color.g = (float)(rand() % 100) / 100;
+        new_color.b = (float)(rand() % 100) / 100;
+        new_color.a = 1;
+        m.color = new_color;
+        tag_color_map[m.id] = new_color;
+      }
+      else
+      {
+        // Old tag; reuse color
+        m.color = tag_color_map[m.id];
+      }
       // lifetime
       m.lifetime = ros::Duration(0); // forever
       // publish
@@ -45,9 +64,13 @@ void msgCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr& msg)
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "talker");
+  ros::init(argc, argv, "marker_publisher");
   ros::NodeHandle nh;
-  ros::Subscriber msg_sub = nh.subscribe("/robosar_agent_bringup_node/agent_1/feedback/apriltag", 1000, msgCallback);
+  std::vector<ros::Subscriber> msg_subscribers;
+  for(size_t agent_idx = 0; agent_idx < agent_names.size(); agent_idx++){
+    const std::string topic_name = topic_name_prefix + agent_names.at(agent_idx) + topic_name_suffix;
+    msg_subscribers.push_back(nh.subscribe(topic_name, 1000, msgCallback));
+  }
   msg_pub = nh.advertise<visualization_msgs::Marker>("victim_markers", 1000);
   ros::spin();
   return 0;
