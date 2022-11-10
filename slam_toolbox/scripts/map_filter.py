544a717ea87b6342e83f645543a1acbf26c24c65
jsongCMU <jaekyuns@andrew.cmu.edu>
2022-11-10 11:46:44 -0500

    Made node publish filtered map

--- slam_toolbox/scripts/map_filter.py
+++ slam_toolbox/scripts/map_filter.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 
 from nav_msgs.msg import OccupancyGrid
 
+pub = rospy.Publisher('/map_filt', OccupancyGrid, queue_size=10)
+
 def filter_map(map_msg):
   scale = map_msg.info.resolution
   origin = [map_msg.info.origin.position.x,
@@ -12,28 +14,17 @@ def filter_map(map_msg):
   # Extract raw map
   raw = np.reshape(
       map_msg.data, (map_msg.info.height, map_msg.info.width))
-  plt.imshow(raw, cmap='gray_r')
-  plt.show()
-  
   # Treat unknown space as occupied space; make binary
   map_filt = np.copy(raw)
   map_filt = np.where(np.logical_or(map_filt<0, map_filt>50), 1, 0)
-  plt.imshow(map_filt, cmap='gray_r')
-  plt.show()
   # Perform opening to remove small islands of occupied space
-  map_filt = ndimage.binary_opening(map_filt, structure=np.ones((3,3))).astype(int)
-  plt.imshow(map_filt, cmap='gray_r')
-  plt.show()
-  # Compare
-  raw_bin = np.copy(raw)
-  raw_bin = np.where(np.logical_or(raw_bin<0, raw_bin>50), 1, 0)
-  diff = np.logical_xor(raw_bin, map_filt)
-  plt.imshow(diff, cmap='gray_r')
-  plt.show()
+  map_filt = ndimage.binary_opening(map_filt, structure=np.ones((3,3))).astype(int) * 100
   # Only update occupied cells
   map_final = np.where(raw > 50, map_filt, raw)
-  plt.imshow(map_final, cmap='gray_r')
-  plt.show()
+  # Publish new map
+  map_msg.data = tuple(map_final.flatten())
+  pub.publish(map_msg)
+
 
 
 if __name__ == '__main__':
